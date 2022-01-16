345ddaff310f31c3711f2867644a126fa8a1e63f
Luke Stoss <luke.stoss@gmail.com>
2022-01-16 12:09:37 -0600

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "/", to: 'homepage#home', as: "root"
-  get "/contact", to: 'contactpage#contact'
-  get "/work", to: 'workpage#work'
-  get "/about", to: 'aboutpage#about'
+  get "contact", to: 'contactpage#contact'
+  get "work", to: 'workpage#work'
+  get "about", to: 'aboutpage#about'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
